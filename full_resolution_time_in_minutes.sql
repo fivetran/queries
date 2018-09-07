3ceb73e8d83702e56d58458f0b560d7fa3ed2afd
Garegin Ordyan <garegin@fivetran.com>
2018-09-07 11:10:04 -0700

    added multiple schedule support replaced [ZENDESK_SCHEMA] with zendesk while we are making changes and testing

--- full_resolution_time_in_minutes.sql
+++ full_resolution_time_in_minutes.sql
@@ -1,18 +1,30 @@
-with ticket_full_solved_time as (
+with ticket_schedule as (
+  select ticket_id,
+         schedule_id,
+         created_at,
+         coalesce(lead(created_at, 1) over ticket_schedule_timeline, timestamp("9999-12-31 01:01:01")) as invalidated_at
+  from zendesk.ticket_schedule
+  window ticket_schedule_timeline as (partition by ticket_id order by created_at)
+),
+ticket_full_solved_time as (
   select ticket.id as ticket_id,
-         ticket.created_at as created_at,
-         round(timestamp_diff(created_at, timestamp_trunc(created_at, week), second)/60, 0) as start_time_in_minutes_from_week,
-         round(timestamp_diff(max(ticket_field_history.updated), created_at, second)/60, 0) as raw_delta_in_minutes
-  from [ZENDESK_SCHEMA].ticket
-  join [ZENDESK_SCHEMA].ticket_field_history on ticket.id = ticket_field_history.ticket_id
+         ticket_schedule.created_at as created_at,
+         ticket_schedule.invalidated_at,
+         ticket_schedule.schedule_id,
+         round(timestamp_diff(ticket_schedule.created_at, timestamp_trunc(ticket_schedule.created_at, week), second)/60, 0) as start_time_in_minutes_from_week,
+         greatest(0, round(timestamp_diff(least(ticket_schedule.invalidated_at, max(ticket_field_history.updated)), ticket_schedule.created_at, second)/60, 0)) as raw_delta_in_minutes
+  from zendesk.ticket
+  left join ticket_schedule on ticket.id = ticket_schedule.ticket_id
+  join zendesk.ticket_field_history on ticket.id = ticket_field_history.ticket_id
   where ticket_field_history.value = 'solved'
-  group by 1, 2
+  group by 1, 2, 3, 4
 ),
 weekly_periods as (
   select ticket_id,
          start_time_in_minutes_from_week,
          raw_delta_in_minutes,
          week_number,
+         schedule_id,
          greatest(0, start_time_in_minutes_from_week - week_number * (7*24*60)) as ticket_week_start_time,
          least(start_time_in_minutes_from_week + raw_delta_in_minutes - week_number * (7*24*60), (7*24*60)) as ticket_week_end_time
   from ticket_full_solved_time, unnest(generate_array(0, floor((start_time_in_minutes_from_week + raw_delta_in_minutes) / (7*24*60)), 1)) as week_number
@@ -20,13 +32,15 @@ weekly_periods as (
 intercepted_periods as (
   select ticket_id,
          week_number,
+         schedule_id,
          ticket_week_start_time,
          ticket_week_end_time,
          schedule.start_time_utc as schedule_start_time,
          schedule.end_time_utc as schedule_end_time,
          least(ticket_week_end_time, schedule.end_time_utc) - greatest(ticket_week_start_time, schedule.start_time_utc) as scheduled_minutes
   from weekly_periods
-  join [ZENDESK_SCHEMA].schedule on ticket_week_start_time <= schedule.end_time_utc and ticket_week_end_time >= schedule.start_time_utc
+  join zendesk.schedule on ticket_week_start_time <= schedule.end_time_utc and ticket_week_end_time >= schedule.start_time_utc
+    and weekly_periods.schedule_id = schedule.id
 ),
 business_minutes as (
   select ticket_id,
@@ -36,8 +50,9 @@ business_minutes as (
   order by 1
 )
 select business_minutes.ticket_id,
-       calendar_minutes.raw_delta_in_minutes as full_resolution_time_in_calendar_minutes,
-       business_minutes.full_resolution_time_in_business_minutes
+       business_minutes.full_resolution_time_in_business_minutes,
+       sum(calendar_minutes.raw_delta_in_minutes) as full_resolution_time_in_calendar_minutes
 from business_minutes
 join ticket_full_solved_time as calendar_minutes
   on business_minutes.ticket_id = calendar_minutes.ticket_id
+group by 1, 2
