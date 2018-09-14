e2767cfe44c30c764ea49940abaeecf871effb4a
gareginordyan <garegin@fivetran.com>
2018-09-13 18:32:19 -0700

    Update ticket_metrics.sql

--- zendesk_ticket_metrics/ticket_metrics.sql
+++ zendesk_ticket_metrics/ticket_metrics.sql
@@ -3,7 +3,7 @@ CREATE VIEW zendesk.ticket_metrics AS (
 WITH group_stations AS (
     SELECT
         ticket_id,
-        COUNT(distinct value) AS number
+        COUNT(distinct value) AS group_stations
     FROM zendesk.ticket_field_history
     WHERE field_name = 'group_id' 
     GROUP BY ticket_id
@@ -11,57 +11,54 @@ WITH group_stations AS (
 assignee_stations AS (
     SELECT
         ticket_id,
-        COUNT(distinct value) AS number
+        COUNT(distinct value) AS assignee_stations
     FROM zendesk.ticket_field_history
     WHERE field_name = 'assignee_id' 
     GROUP BY ticket_id
-),
-grouped_ticket_status_history AS ( 
-    SELECT * 
-    FROM zendesk.ticket_field_history
-    WHERE field_name = 'status' 
-    ORDER BY ticket_id, updated
 ),
 ticket_status_changes AS ( 
     SELECT 
         ticket_id,
-        LAG(ticket_id, 1, 0) OVER(ORDER BY ticket_id, updated) AS prev_ticket_id, 
         value AS status, 
-        LAG(value, 1, 'new') OVER(ORDER BY ticket_id, updated) AS prev_status
-    FROM grouped_ticket_status_history
+        LAG(value, 1) OVER (PARTITION BY ticket_id ORDER BY updated) AS prev_status
+    FROM zendesk.ticket_field_history
+    WHERE field_name = 'status' 
 ),
 reopens AS (
     SELECT 
-        DISTINCT ticket_id, 
+        ticket_id, 
         COUNT(ticket_id) AS reopens
     FROM ticket_status_changes 
-    WHERE ticket_id = prev_ticket_id AND prev_status = 'solved' AND status = 'open' 
+    WHERE prev_status = 'solved' AND status = 'open' 
     GROUP BY ticket_id
 ),
 replies AS (
     SELECT
-        DISTINCT ticket_id,
+        ticket_id,
         COUNT(ticket_id) AS replies
     FROM zendesk.ticket_comment
-    WHERE public
+    JOIN zendesk.user
+      ON ticket_comment.user_id = user.id
+    WHERE user.role IN ('agent', 'admin') AND public
     GROUP BY ticket_id
 ),
 assignee_updated_at AS (
     SELECT
-        ticket_id,
-        MAX(updated) AS assignee_updated_at
-    FROM zendesk.ticket_field_history
-    WHERE field_name = 'assignee_id' 
-    GROUP BY ticket_id
-
+        ticket.id as ticket_id,
+        max(updated) as assignee_updated_at
+    FROM zendesk.ticket
+    JOIN zendesk.ticket_field_history 
+      ON ticket_field_history.ticket_id = ticket.id AND ticket_field_history.user_id = ticket.assignee_id
+    GROUP BY 1
 ),
 requester_updated_at AS (
     SELECT
-        ticket_id,
-        MAX(updated) AS requester_updated_at
-    FROM zendesk.ticket_field_history
-    WHERE field_name = 'requester_id' 
-    GROUP BY ticket_id
+        ticket.id as ticket_id,
+        max(updated) as requester_updated_at
+    FROM zendesk.ticket
+    JOIN zendesk.ticket_field_history 
+      ON ticket_field_history.ticket_id = ticket.id AND ticket_field_history.user_id = ticket.requester_id
+    GROUP BY 1
 ),
 status_updated_at AS (
     SELECT
@@ -105,7 +102,7 @@ latest_comment_added_at AS (
 SELECT ticket.id AS ticket_id,
     group_stations.group_stations,
     assignee_stations.assignee_stations,
-    reopens.reopens,
+    coalesce(reopens.reopens, 0) as reopens,
     replies.replies,
     assignee_updated_at.assignee_updated_at,
     requester_updated_at.requester_updated_at,
@@ -114,12 +111,12 @@ SELECT ticket.id AS ticket_id,
     assigned_at.assigned_at,
     solved_at.solved_at,
     latest_comment_added_at.latest_comment_added_at,
-    first_response_time.first_resoponse_time_in_calendar_minutes,
-    first_response_time.first_resoponse_time_in_business_minutes,
-    first_resolution.first_resolution_time_in_calendar_minutes,
-    first_resolution.first_resolution_time_in_business_minutes,
-    full_resolution.full_resolution_time_in_calendar_minutes,
-    full_resolution.full_resolution_time_in_business_minutes,
+    first_response_time.first_resoponse_time_in_calendar_minutes as reply_time_in_calendar_minutes,
+    first_response_time.first_resoponse_time_in_business_minutes as reply_time_in_business_minutes,
+    first_resolution_time.first_resolution_time_in_calendar_minutes,
+    first_resolution_time.first_resolution_time_in_business_minutes,
+    full_resolution_time.full_resolution_time_in_calendar_minutes,
+    full_resolution_time.full_resolution_time_in_business_minutes,
     wait_times.agent_wait_time_in_calendar_minutes,
     wait_times.agent_wait_time_in_business_minutes,
     wait_times.requester_wait_time_in_calendar_minutes,
@@ -128,7 +125,7 @@ SELECT ticket.id AS ticket_id,
     wait_times.on_hold_time_in_business_minutes
 FROM zendesk.ticket
 LEFT JOIN group_stations ON ticket.id = group_stations.ticket_id
-LEFT JOIN assignee_stations oONn ticket.id = assignee_stations.ticket_id
+LEFT JOIN assignee_stations ON ticket.id = assignee_stations.ticket_id
 LEFT JOIN reopens ON ticket.id = reopens.ticket_id
 LEFT JOIN replies ON ticket.id = replies.ticket_id
 LEFT JOIN assignee_updated_at ON ticket.id = assignee_updated_at.ticket_id
@@ -137,9 +134,9 @@ LEFT JOIN status_updated_at ON ticket.id = status_updated_at.ticket_id
 LEFT JOIN initially_assigned_at ON ticket.id = initially_assigned_at.ticket_id
 LEFT JOIN assigned_at ON ticket.id = assigned_at.ticket_id
 LEFT JOIN solved_at ON ticket.id = solved_at.ticket_id
-LEFT JOIN latest_comment_added_at ON ticket.id = latest_comment_added_at.id
-LEFT JOIN first_response_time ON ticket.id = first_response_time.ticket_id
-LEFT JOIN first_resolution_time ON ticket.id = first_resolution_time.ticket_id
-LEFT JOIN full_resolution_time ON ticket.id = full_resolution_time.ticket_id
-LEFT JOIN wait_times ON ticket.id = wait_times.ticket_id 
+LEFT JOIN latest_comment_added_at ON ticket.id = latest_comment_added_at.ticket_id
+LEFT JOIN zendesk.first_response_time ON ticket.id = first_response_time.ticket_id
+LEFT JOIN zendesk.first_resolution_time ON ticket.id = first_resolution_time.ticket_id
+LEFT JOIN zendesk.full_resolution_time ON ticket.id = full_resolution_time.ticket_id
+LEFT JOIN zendesk.wait_times ON ticket.id = wait_times.ticket_id
 );
